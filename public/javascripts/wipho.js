ea4be3eeba4e451a9d0c62aee349e677f43ff6cc
Jan Grewe <jan@faked.org>
2015-03-08 19:23:54 +0100

    improve webinterface design

--- public/javascripts/wipho.js
+++ public/javascripts/wipho.js
@@ -1,18 +1,44 @@
 $(document).ready(function() {
 
-    $('body').height($(window).height());
+  $('body').height($(window).height());
+  centerImage();
 
-	getLatest();
-	setInterval(function() {
-		getLatest();
-	}, 3000);
-	
+  getLatest();
+  setInterval(function() {
+    getLatest();
+  }, 1000);
+
+  $(window).resize(function () {
+    centerImage();
+  });
+
+  $('#latest').one('load', function(){
+    $(this).one('load', function(){
+      $(this).addClass('imgBorder');
+    });
+  });
+
+  $('#latest').on('load', function(){
+    centerImage();
+    $(this).animate({opacity: 1}, 400);
+  });
+  
 });
 
 function getLatest() {
-	$.getJSON('/latest', function(data) {
-		if('/previews/'+data.name != $('#latest').attr('src')) {
-			$('#latest').attr('src', '/previews/'+data.name);
-		}
-	});
+  $.getJSON('/latest', function(data) {
+    if('/previews/'+data.name != $('#latest').attr('src')) {
+      $('#latest').animate({opacity: 0}, 400, function() {
+        $(this).attr('src', '/previews/'+data.name);
+      });
+    }
+  });
+}
+
+function centerImage() {
+  $('#latest').css({
+    position: 'absolute',
+    left: ($(window).width() - $('#latest').outerWidth()) / 2,
+    top: ($(window).height() - $('#latest').outerHeight()) / 2,
+  });
 }
